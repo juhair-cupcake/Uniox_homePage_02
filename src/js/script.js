88d5d39e9121205a2a412e5ad4ac580ac5eb5cc5
error6251 <juhair98@gmail.com>
2020-11-17 13:34:38 +0600

    added: sponsors section updated: schedule

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 //Make the Schedule Dynamic
-function openCity(evt, cityName) {
+function openSchedule(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -13,5 +13,29 @@ function openCity(evt, cityName) {
   evt.currentTarget.className += " active";
 }
 
+//Make the Sponsors Section Dynamic
+//Sponsors tab for
+function showSponsors(evt, name) {
+  //Make All link and logo inactive
+  let tablinks = document.getElementsByClassName("sponsorsLink");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  let tabInactive = document.getElementsByClassName("sponsorslogo");
+  for (let i = 0; i < tabInactive.length; i++) {
+    tabInactive[i].className = tabInactive[i].className.replace(" active", "");
+  }
+
+  //Make the selected logo active
+  let tabcontent = document.getElementsByClassName(name);
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].className += " active";
+  }
+
+  evt.currentTarget.className += " active";
+}
+
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen1").click();
+document.getElementById("defaultOpen2").click();
