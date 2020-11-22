0aa33ad5e5ab7e1b33691fff9142e7fc64259683
error6251 <juhair98@gmail.com>
2020-11-22 18:36:49 +0600

    added: location Section

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 //Make the Schedule Dynamic
 function openSchedule(evt, cityName) {
-  var i, tabcontent, tablinks;
+  let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
@@ -68,6 +68,22 @@ function secondUpdate() {
   document.getElementById("Seconds").innerHTML = seconds;
 }
 
+//Make the MAP Section Dynamic
+function openMap(evt, Name) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("mapcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("maplinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(Name).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen1").click();
 document.getElementById("defaultOpen2").click();
+document.getElementById("defaultOpen3").click();
